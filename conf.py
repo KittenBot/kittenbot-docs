1143a3db140ace8f0557fddd1a30bb7a89e2e065
hogan96 <234513426@qq.com>
2020-02-12 11:07:56 +0800

    fix

--- conf.py
+++ conf.py
@@ -41,7 +41,7 @@ from recommonmark.transform import AutoStructify
 # If your documentation needs a minimal Sphinx version, state it here.
 #
 
-needs_sphinx = '2.4.0'
+# needs_sphinx = '2.4.0'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
